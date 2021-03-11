ffc403ed459d3a8e3447a549958dc494b26d3f12
Valery Akimov <valery.e.akimov@gmail.com>
2021-03-12 02:08:07 +0300

    Add: parse scheduling

---
+++ src/main/java/com/lookfor/iwannatravel/services/ParseScheduler.java
@@ -0,0 +1,30 @@
+package com.lookfor.iwannatravel.services;
+
+import com.lookfor.iwannatravel.parsers.OneToTripParser;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+import org.springframework.jmx.export.annotation.ManagedResource;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ExecutionException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+@ManagedResource(objectName = "ParseMBeans:category=MBeans,name=ParseScheduler")
+public class ParseScheduler {
+    private final OneToTripParser oneToTripParser;
+
+    /**
+     * Parse every 1 hour
+     * @throws ExecutionException exp
+     * @throws InterruptedException exp
+     */
+    @Scheduled(cron = "0 0 0/1 1/1 * ?")
+    @ManagedOperation(description = "Parse web page for traveling restrictions")
+    public void parseWebPage() throws ExecutionException, InterruptedException {
+        log.info(String.valueOf(oneToTripParser.getResult().get()));
+    }
+}
