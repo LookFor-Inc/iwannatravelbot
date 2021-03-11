7b5cebfa6f5ccfd89ddc48404d288ef77b7b75a6
Valery Akimov <valery.e.akimov@gmail.com>
2021-03-11 21:02:44 +0300

    Init

---
+++ src/main/java/com/lookfor/iwannatravel/IWannaTravelApplication.java
@@ -0,0 +1,13 @@
+package com.lookfor.iwannatravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IWannaTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IWannaTravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lookfor/iwannatravel/IWannaTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lookfor.iwannatravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IWannaTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
